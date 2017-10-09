97bf2b5a8f39fd2510c6b34a5ea1f92c4d10f3a2
Vedavidh <vedavidhbudimuri@gmail.com>
2017-10-09 14:13:18 +0530

    next@3

---
+++ next.py
@@ -0,0 +1,12 @@
+"""
+    Next module
+"""
+
+
+class NextClass(object):
+    """
+        This is a sample class to create
+    """
+    def __init__(self, *args, **kwargs):
+        self._value = kwargs.get('value', 'value')
+
